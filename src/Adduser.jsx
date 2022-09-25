3fe0dc9dd89d518fe1029c7d4dd8c66f88aabb9b
ashukla33317 <amanshukla33317@gmail.com>
2022-09-25 14:33:56 +0530

    first commit

--- src/Adduser.jsx
+++ src/Adduser.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect,useCallBack } from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
--- src/HomeData.jsx
+++ src/HomeData.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Link,useNavigate } from 'react-router-dom';
-import UpdateUser from './UpdateUser';
+import { useNavigate } from 'react-router-dom';
 
 
 const HomeData = (props) => {
